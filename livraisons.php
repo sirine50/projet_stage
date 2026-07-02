4980982db9ef911d54dd664024416e67f958f2ad
haritybasma <haritybasma@gmail.com>
2026-07-02 22:30:41 +0100

    guh

--- livraisons.php
+++ livraisons.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livraisons</title>
+    <link rel="stylesheet" href="sidebar_header.css">
     <link rel="stylesheet" href="livraisons.css">
     <link rel="stylesheet" href="main_components.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
